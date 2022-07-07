f568ab1802a75c9158efcacd1e1ed92853209897
bruk19 <brukteshome21@gmail.com>
2022-07-07 13:37:43 +0300

    Update the navbar to show the links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
       <>
-        <main>
-          
-        </main>
+        <h1>Bookstore CMS</h1>
+        <div className="nav-links">
+          <Link className="links" to="/Books">
+            Books
+          </Link>
+          <Link className="links" to="/Categories">
+            Categories
+          </Link>
+        </div>
       </>
     );
   }
