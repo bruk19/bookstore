a5cd330679c00e5fcb8dda8798e2bd4983a942ea
bruk19 <brukteshome21@gmail.com>
2022-07-11 22:08:30 +0300

    Add books reducer, action type and action creators

--- src/redux/books/Books.js
+++ src/redux/books/Books.js
@@ -1,12 +1,31 @@
-import React from 'react'
+const ADD_BOOK = 'ADD_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
 
-function Books() {
-    return (
-      <>
-        <main>
-          
-        </main>
-      </>
-    );
+const initialState = [];
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return [
+        ...state.filter((book) => book.id !== action.payload),
+      ];
+    default:
+      return state;
   }
-export default Books;
+}
+
+export function addBook(book) {
+  return {
+    type: ADD_BOOK,
+    payload: book,
+  };
+}
+
+export function removeBook(id) {
+  return {
+    type: REMOVE_BOOK,
+    payload: id,
+  };
+}
