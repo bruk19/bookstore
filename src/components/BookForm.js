5d19c01be16f819c42ea01cad1082c156be30b2b
bruk19 <brukteshome21@gmail.com>
2022-07-13 14:01:52 +0300

    Implement adding a book using the form

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-function BookForm() {
-    return (
-      <>
+const BookForm = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBook = {
+      author,
+      title,
+      id: new Date().getMilliseconds(),
+    };
+    dispatch(addBook(newBook));
+  };
+
+  return (
+    <div className="book form">
       <h2>ADD NEW BOOK</h2>
-    <form>
-      <input type="text" name="title" placeholder="Book title" />
-      <input type="text" name="author" placeholder="Author" />
-      <button type="submit">ADD BOOK</button>
-    </form>
-      </>
-    );
-  }
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="title" placeholder="Book title" onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" name="author" placeholder="Author" onChange={(e) => setAuthor(e.target.value)} />
+        <button type="submit">ADD BOOK</button>
+      </form>
+    </div>
+  );
+};
+
 export default BookForm;
