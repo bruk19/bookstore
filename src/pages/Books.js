7b3e6d3a9bb32bffe3a88e0b2cb724cedcc84fda
bruk19 <brukteshome21@gmail.com>
2022-07-14 17:34:23 +0300

    Implement loading books

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,11 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Book from '../components/Book';
 import React from 'react';
 import BookForm from '../components/BookForm';
+import { loadBooks } from '../redux/books/books';
 
 
 const Books = () => {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadBooks);
+  }, []);
 
   return (
     <>
--- src/redux/books/Books.js
+++ src/redux/books/Books.js
@@ -1,21 +1,13 @@
 const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
+const BOOKS_LOADED = 'BOOKS_LOADED';
 
-const initialState = [
-  {
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    id: 'ashda9ysudhasd0',
-  },
-  {
-    title: 'Dune',
-    author: 'Frank Herbert',
-    id: 'asdahsd9qhwdoa0a9wq',
-  },
-];
+const initialState = [];
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
+    case BOOKS_LOADED:
+      return action.payload;
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
@@ -39,4 +31,26 @@ export function removeBook(id) {
     type: REMOVE_BOOK,
     payload: id,
   };
+}
+
+export function booksLoaded(books) {
+  return {
+    type: BOOKS_LOADED,
+    payload: books,
+  };
+}
+
+export async function loadBooks(dispatch) {
+  try {
+    const result = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/dXhbHW84R2UylfqfKuq7/books');
+    const data = await result.json();
+    const books = Object.keys(data).map((key) => ({
+      id: key,
+      ...data[key][0],
+    }));
+
+    dispatch(booksLoaded(books));
+  } catch (error) {
+    dispatch(booksLoaded([]));
+  }
 }
